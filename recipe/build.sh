9ca948f93f61bf9cc1e74766bb55fcdd46557bb2
Billy Suh <bsuh@nvidia.com>
2024-11-06 19:02:44 +0000

    Add target libraries/executables to executable's RPATH

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,6 @@ mkdir -p ${PREFIX}/lib/pkgconfig
 mkdir -p ${PREFIX}/gds
 rm -rv etc
 mv -v man ${PREFIX}/man
-mv -v tools ${PREFIX}/gds/tools
 [[ -d pkg-config ]] && mv pkg-config/* ${PREFIX}/lib/pkgconfig/
 [[ -d "$PREFIX/lib/pkgconfig" ]] && sed -E -i "s|cudaroot=.+|cudaroot=$PREFIX|g" $PREFIX/lib/pkgconfig/cufile*.pc
 
@@ -18,20 +17,35 @@ for i in `ls`; do
     [[ $i == "build_env_setup.sh" ]] && continue
     [[ $i == "conda_build.sh" ]] && continue
     [[ $i == "metadata_conda_debug.yaml" ]] && continue
-    if [[ $i == "lib" ]] || [[ $i == "gds" ]] || [[ $i == "include" ]]; then
+
+    if [[ $i == "lib" ]] || [[ $i == "include" ]]; then
         mkdir -p ${PREFIX}/${targetsDir}
         mkdir -p ${PREFIX}/$i
         cp -rv $i ${PREFIX}/${targetsDir}
         if [[ $i == "lib" ]]; then
             for j in "$i"/*.so*; do
+                echo j = $j
+                [[ -L ${PREFIX}/$j ]] && continue
+
                 # Shared libraries are symlinked in $PREFIX/lib
+                echo ln -s ${PREFIX}/${targetsDir}/$j ${PREFIX}/$j
                 ln -s ${PREFIX}/${targetsDir}/$j ${PREFIX}/$j
 
-                if [[ $j =~ \.so\. ]]; then
-                    patchelf --set-rpath '$ORIGIN' --force-rpath ${PREFIX}/${targetsDir}/$j
-                fi
+                echo patchelf --set-rpath '$ORIGIN' --force-rpath ${PREFIX}/${targetsDir}/$j
+                patchelf --set-rpath '$ORIGIN' --force-rpath ${PREFIX}/${targetsDir}/$j
             done
         fi
+    elif [[ $i == "tools" ]]; then
+        cp -rv $i ${PREFIX}/gds/
+        for j in `find $PREFIX/gds/$i`; do
+            echo j = $j
+            [[ -f $j ]] || continue
+            k=`basename $j`
+            if [[ $k == "gdscp" ]] || [[ $k == "gdsio" ]] || [[ $k == "gdscheck" ]] || [[ $k == "gds_stats" ]] || [[ $k == "gdsio_verify" ]]; then
+                echo patchelf --set-rpath "\$ORIGIN/../../lib:\$ORIGIN/../../${targetsDir}/lib" --force-rpath $j
+                patchelf --set-rpath "\$ORIGIN/../../lib:\$ORIGIN/../../${targetsDir}/lib" --force-rpath $j
+            fi
+        done
     else
         # Put all other files in targetsDir
         mkdir -p ${PREFIX}/${targetsDir}/${PKG_NAME}
